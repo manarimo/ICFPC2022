9c9a7cb187c7f86904b9c8571fabe307829ee978
kenkoooo <kenkou.n@gmail.com>
2022-09-03 05:48:54 +0900

    remove unused hooks

--- object-object-visualizer/src/hooks/useSolution.tsx
+++
@@ -1,7 +0,0 @@
-import useSWR from "swr";
-
-export const useSolution = (solver: string, problemId: number) => {
-  return useSWR<{ code: string }>(
-    `https://gxtbs67iyup735zlzm54b6574i0pmpwl.lambda-url.us-east-1.on.aws/api/solution?batch=${solver}&problemId=${problemId}`
-  );
-};
--- object-object-visualizer/src/hooks/useSolverList.tsx
+++
@@ -1,7 +0,0 @@
-import useSWR from "swr";
-
-export const useSolverList = () => {
-  return useSWR<{ items: string[] }>(
-    "https://gxtbs67iyup735zlzm54b6574i0pmpwl.lambda-url.us-east-1.on.aws/api/batches"
-  );
-};
