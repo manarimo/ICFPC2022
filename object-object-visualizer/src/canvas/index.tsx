1c9a6e38754bd1596ba12bb59571f2b718306c06
kenkoooo <kenkou.n@gmail.com>
2022-09-03 05:31:13 +0900

    refactoring

---
+++ object-object-visualizer/src/canvas/index.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useRef } from "react";
+
+interface Props {
+  width: number;
+  height: number;
+  getColor: (
+    x: number,
+    y: number
+  ) => { r: number; g: number; b: number; a: number };
+}
+export const Canvas = ({ width, height, getColor }: Props) => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas?.getContext("2d");
+    if (ctx) {
+      ctx.fillStyle = "rgb(255,255,255)";
+      ctx.fillRect(0, 0, width, height);
+      for (let x = 0; x < width; x++) {
+        for (let y = 0; y < height; y++) {
+          const { r, g, b, a } = getColor(x, y);
+          ctx.fillStyle = `rgba(${r},${g},${b},${a})`;
+          ctx.fillRect(x, height - y - 1, 1, 1);
+        }
+      }
+    }
+  }, [canvasRef, width, height, getColor]);
+  return (
+    <canvas
+      style={{ margin: "10px" }}
+      width={width}
+      height={height}
+      ref={canvasRef}
+    ></canvas>
+  );
+};
