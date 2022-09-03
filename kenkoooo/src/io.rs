c67b7c0e6e9d9ec922f84da7ee2fab1e1651991f
soboku <you@example.com>
2022-09-04 08:45:28 +1000

    kenkoooo io taioooo

--- kenkoooo/src/io.rs
+++ kenkoooo/src/io.rs
@@ -1,6 +1,9 @@
 use std::{fs::read_to_string, path::Path};
 
 use crate::{scanner::IO, types::Picture};
+use crate::ops::color::Color;
+use crate::ops::Move;
+use crate::types::{Block, Label, RGBA, State};
 
 pub fn read_input<P: AsRef<Path>>(path: P) -> Picture {
     let input = read_to_string(path).unwrap();
@@ -16,6 +19,25 @@ pub fn read_input<P: AsRef<Path>>(path: P) -> Picture {
                 rgba[i][j].0[color] = sc.read();
             }
         }
+    }
+
+    let mut state = State::new(w, h);
+    state.pop_block(&Label(vec![0]));
+
+    let blocks: usize = sc.read();
+    for _i in 0..blocks {
+        let id: u32 = sc.read();
+        let x1: usize = sc.read();
+        let y1: usize = sc.read();
+        let x2: usize = sc.read();
+        let y2: usize = sc.read();
+        let r: u8 = sc.read();
+        let g: u8 = sc.read();
+        let b: u8 = sc.read();
+        let a: u8 = sc.read();
+        state.push_block(Label(vec![id]), Block{x1, y1, x2, y2});
+        let color_move = Move::Color(Color{ color: RGBA([r, g, b, a]), label: Label(vec![id])});
+        state.apply(color_move);
     }
 
     Picture(rgba)
