668c309bf926d1dc4c737b5592744bb0090764ef
Osamu Koga (osa_k) <osak.63@gmail.com>
2022-09-02 20:34:02 -0500

    解答リスト

--- object-object-visualizer/src/Top.tsx
+++ object-object-visualizer/src/Top.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 import useSWR from "swr";
+import { Link } from "react-router-dom";
+import "./Top.css";
 
 async function fetcher<T>(path: string): Promise<T> {
   const response = await fetch(
@@ -8,33 +10,52 @@ async function fetcher<T>(path: string): Promise<T> {
   return response.json();
 }
 
-interface Batches {
-  items: string[];
+interface SolutionsAll {
+  solutions: BatchSpec[];
+}
+
+interface BatchSpec {
+  batchName: string;
+  solutions: Solution[];
+}
+
+interface Solution {
+  batchName: string;
+  problemId: string;
 }
 
 export function Top() {
-  const { data, error } = useSWR<Batches, Error>("api/batches", fetcher);
+  const { data, error } = useSWR<SolutionsAll, Error>(
+    "api/solution_all",
+    fetcher
+  );
 
   if (data === undefined) {
     return null;
   }
 
   return (
-    <div style={{ display: "flex" }}>
-      <table style={{ margin: "auto" }}>
-        <thead>
-          <tr>
-            <th>Batch name</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.items.map((item) => (
-            <tr key={item}>
-              <td>{item}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      {data.solutions.map((batch) => renderBatchPane(batch))}
+    </div>
+  );
+}
+
+function renderBatchPane(batchSpec: BatchSpec) {
+  return (
+    <div className="batch-pane">
+      <h2 className="batch-name">{batchSpec.batchName}</h2>
+      <div className="batch-solutions-list">
+        {batchSpec.solutions.map((sol) => (
+          <div className="batch-solution-link">
+            <Link
+              to={`/vis?batchName=${sol.batchName}&problemId=${sol.problemId}`}
+            >
+              {sol.problemId}
+            </Link>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
