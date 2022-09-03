5b1af3ee95b4e7890553acf31b84057cb665b49a
Osamu Koga (osa_k) <osak.63@gmail.com>
2022-09-03 00:24:41 -0500

    add writeSolutionImage function

--- object-object-visualizer/scorer/index.ts
+++ object-object-visualizer/scorer/index.ts
@@ -73,6 +73,31 @@ async function doScoring(problemPngFile: string, solutionFile: string): Promise<
     return calculateScore(problem, solutionOutput);
 }
 
+async function writeSolutionImage(state: State, destFile: string): Promise<void> {
+    const png = new PNG({
+        width: state.width,
+        height: state.height,
+    });
+    const numPx = state.width * state.height;
+    for (let px = 0; px < numPx; px++) {
+        const pngX = px % state.width;
+        const pngY = state.height - Math.floor(px / state.width) - 1;
+        const pngPx = pngY * state.width + pngX;
+        png.data[pngPx + 0] = state.r[px];
+        png.data[pngPx + 1] = state.g[px];
+        png.data[pngPx + 2] = state.b[px];
+        png.data[pngPx + 3] = state.a[px];
+    }
+
+    const fileHandle = await fsPromises.open(destFile, 'w');
+    return new Promise((resolve, reject) => {
+        const packed = png.pack();
+        packed.on('end', () => resolve());
+        packed.on('error', (err) => reject(err));
+        packed.pipe(fileHandle.createWriteStream());
+    });
+}
+
 async function* dirEntries(path: string) {
     const dirHandle = await fsPromises.opendir(path);
     while(true) {
