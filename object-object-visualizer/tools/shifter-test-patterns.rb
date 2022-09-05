b898245326897c0fa173291490ca046d3587820b
Osamu Koga (osa_k) <osak.63@gmail.com>
2022-09-05 01:49:27 -0500

    Shifter 22

--- object-object-visualizer/tools/shifter-test-patterns.rb
+++ object-object-visualizer/tools/shifter-test-patterns.rb
@@ -9,11 +9,13 @@ pattern = [
   Pattern.new(15, [0, 2], [4]),
   Pattern.new(18, [0, 1, 2], [3, 4, 5]),
   Pattern.new(20, [1, 2, 3], [3, 4, 5]),
+  Pattern.new(22, [0, 2], [5, 6]),
   Pattern.new(24, [0, 1, 2, 3], [4, 5])
 ]
 
 rules = []
 pattern.each do |pat|
+  next if pat.probId != 22
   pat.rotates.each do |r|
     pat.splits.each do |s|
       pat_name = "rot#{r}-sp#{s}"
