bde7a336d4c7e1451e19869614da7d168fc62fe1
Mirai Towa <mkut.at@gmaill.com>
2022-09-05 18:18:28 +0900

    manual shifter

--- object-object-visualizer/tools/manual_shifter/index.ts
+++ object-object-visualizer/tools/manual_shifter/index.ts
@@ -2,15 +2,28 @@ import { Input, Output, Processor } from '../metaprocessor';
 import { Image, runSolution } from '../util';
 import { Move } from '../../src/parser';
 
+function swap(arr: Uint8Array, i: number, j: number) {
+    const tmp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = tmp;
+}
+
 export class ManualShifter implements Processor {
-    readonly splits: [string, number, number][] = [];
+    readonly splits: ['x' | 'y', number, number][] = [];
+    readonly swaps: ['x' | 'y', number, number, number, number][] = [];
+    globalCounter: number = 0;
+
     constructor(splits: string) {
         const nums = splits.split(',');
         if (nums.length % 3 != 0) {
             throw new Error('manual shifter: args must be 3x');
         }
         for (let i = 0; i < nums.length / 3; i++) {
-            this.splits.push([nums[i * 3], parseInt(nums[i * 3 + 1]), parseInt(nums[i * 3 + 2])]);
+            let ori = nums[i * 3];
+            if (ori !== 'x' && ori !== 'y') {
+                throw new Error(`manual shifter: invalid orientation ${ori}`);
+            }
+            this.splits.push([ori, parseInt(nums[i * 3 + 1]), parseInt(nums[i * 3 + 2])]);
         }
         this.validate();
     }
@@ -25,7 +38,18 @@ export class ManualShifter implements Processor {
         for (const split of this.splits) {
             comments.push({ kind: 'comment-move', comment: `manual_shifter: split at ${split[0]} ${split[1]} ${split[2]}` });
         }
-        const moves: Move[] = [...comments, ...output.moves, ...this.shiftMoves(finalState.globalCounter, input)];
+        this.globalCounter = finalState.globalCounter;
+        const moves: Move[] = [...comments, ...output.moves, ...this.shiftMoves(input)];
+
+        /*
+        console.log('P3 400 400 255');
+        for (let y = 0; y < 400; y++) {
+            for (let x = 0; x < 400; x++) {
+                console.log(newImage.r[x + y * newImage.width], newImage.g[x + y * newImage.width], newImage.b[x + y * newImage.width]);
+            }
+        }
+        */
+
         return new Output(moves);
     }
 
@@ -42,6 +66,7 @@ export class ManualShifter implements Processor {
             } else {
                 y_acc += end - begin;
             }
+            this.swaps.push([ori, ori === 'x' ? x_acc : y_acc, ori === 'x' ? x_acc + (end - begin) : y_acc + (end - begin), begin, end]);
         }
     }
 
@@ -82,19 +107,11 @@ export class ManualShifter implements Processor {
                     const newPx = newX + newY * image.width;
                     const imgPx = oldX + oldY * image.width;
                     let tmp;
-                    tmp = nImg.r[newPx];
-                    nImg.r[newPx] = nImg.r[imgPx];
-                    nImg.r[imgPx] = tmp;
-                    tmp = nImg.g[newPx];
-                    nImg.g[newPx] = nImg.g[imgPx];
-                    nImg.g[imgPx] = tmp;
-                    tmp = nImg.b[newPx];
-                    nImg.b[newPx] = nImg.b[imgPx];
-                    nImg.b[imgPx] = tmp;
-                    tmp = nImg.a[newPx];
-                    nImg.a[newPx] = nImg.a[imgPx];
-                    nImg.a[imgPx] = tmp;
-                    console.log(newX, newY, nImg.r[imgPx], oldX, oldY, nImg.r[newPx]);
+                    swap(nImg.r, newPx, imgPx);
+                    swap(nImg.g, newPx, imgPx);
+                    swap(nImg.b, newPx, imgPx);
+                    swap(nImg.a, newPx, imgPx);
+                    // console.log(newX, newY, nImg.r[imgPx], oldX, oldY, nImg.r[newPx]);
                 }
             }
             if (ori === 'x') {
@@ -102,14 +119,64 @@ export class ManualShifter implements Processor {
             } else {
                 y_acc += end - begin;
             }
-            console.log(x_acc, y_acc);
+            // console.log(x_acc, y_acc);
         }
 
         return nImg;
     }
 
-    private shiftMoves(globalCounter: number, input: Input): Move[] {
-        return [];
+    private lcutMove(blockId: string, orientation: 'x' | 'X' | 'y' | 'Y', lineNumber: number): Move {
+        return { kind: 'lcut-move', blockId, orientation, lineNumber };
+    }
+
+    private mergeMove(blockId1: string, blockId2: string): Move {
+        this.globalCounter++;
+        return { kind: 'merge-move', blockId1, blockId2 };
+    }
+
+    private shiftMoves(input: Input): Move[] {
+        let currentBlock = `${this.globalCounter}`;
+        const blockIds = [];
+        const moves: Move[] = [];
+        const swapBlockIds = [];
+        for (let i = this.swaps.length - 1; i >= 0; i--) {
+            const [ori, begin1, end1, begin2, end2] = this.swaps[i];
+            if (begin1 !== 0) {
+                moves.push(this.lcutMove(currentBlock, ori, begin1));
+                blockIds.push(`${currentBlock}.0`);
+                currentBlock = `${currentBlock}.1`;
+            }
+            moves.push(this.lcutMove(currentBlock, ori, end1));
+            blockIds.push(`${currentBlock}.0`);
+            swapBlockIds.push(`${currentBlock}.0`);
+            currentBlock = `${currentBlock}.1`;
+            moves.push(this.lcutMove(currentBlock, ori, begin2));
+            blockIds.push(`${currentBlock}.0`);
+            currentBlock = `${currentBlock}.1`;
+            if (end2 !== 400) {
+                moves.push(this.lcutMove(currentBlock, ori, end2));
+                blockIds.push(`${currentBlock}.0`);
+                swapBlockIds.push(`${currentBlock}.0`);
+                currentBlock = `${currentBlock}.1`;
+            } else {
+                swapBlockIds.push(`${currentBlock}`);
+            }
+            blockIds.push(currentBlock);
+            moves.push({ kind: 'swap-move', blockId1: swapBlockIds[0], blockId2: swapBlockIds[1] });
+            let block = blockIds[0];
+            for (let j = 1; j < blockIds.length; j++) {
+                let block2 = blockIds[j];
+                if (block2 === swapBlockIds[0]) {
+                    block2 = swapBlockIds[1];
+                } else if (block2 === swapBlockIds[1]) {
+                    block2 = swapBlockIds[0];
+                }
+                moves.push(this.mergeMove(block, block2));
+                block = `${this.globalCounter}`;
+            }
+        }
+
+        return moves;
         /*
         const blockIds = [];
         const moves: Move[] = [];
