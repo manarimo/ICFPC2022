7087bc1d59b71bdfacaf9b736df8202a2cf33ef8
soboku <you@example.com>
2022-09-05 10:05:43 +1000

    comment keep

---
+++ kenkoooo/src/ops/comment.rs
@@ -0,0 +1,11 @@
+use std::fmt::Display;
+
+#[derive(Clone)]
+pub struct Comment {
+    pub comment: String,
+}
+impl Display for Comment {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.comment)
+    }
+}
